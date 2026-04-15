fad29c7360e0588cb9658e2e33ca65f2da62588e
MatthewLuo7 <matthewluo7@163.com>
2026-04-15 12:12:57 -0600

    upload scripts

---
+++ exp/percolation_points/scripts/run_1.sh
@@ -0,0 +1,97 @@
+# line2line, r=0.45
+python ./line2line_point_sim.py -x 30.0 -y 30.0 -z  4.0 -r 0.45 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/line2line/
+python ./line2line_point_sim.py -x 30.0 -y 30.0 -z  5.0 -r 0.45 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/line2line/
+python ./line2line_point_sim.py -x 30.0 -y 30.0 -z  6.0 -r 0.45 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/line2line/
+python ./line2line_point_sim.py -x 30.0 -y 30.0 -z  7.0 -r 0.45 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/line2line/
+python ./line2line_point_sim.py -x 30.0 -y 30.0 -z  8.0 -r 0.45 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/line2line/
+python ./line2line_point_sim.py -x 30.0 -y 30.0 -z  9.0 -r 0.45 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/line2line/
+python ./line2line_point_sim.py -x 30.0 -y 30.0 -z 10.0 -r 0.45 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/line2line/
+python ./line2line_point_sim.py -x 30.0 -y 30.0 -z 11.0 -r 0.45 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/line2line/
+python ./line2line_point_sim.py -x 30.0 -y 30.0 -z 12.0 -r 0.45 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/line2line/
+python ./line2line_point_sim.py -x 30.0 -y 30.0 -z 13.0 -r 0.45 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/line2line/
+python ./line2line_point_sim.py -x 30.0 -y 30.0 -z 14.0 -r 0.45 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/line2line/
+python ./line2line_point_sim.py -x 30.0 -y 30.0 -z 15.0 -r 0.45 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/line2line/
+python ./line2line_point_sim.py -x 30.0 -y 30.0 -z 16.0 -r 0.45 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/line2line/
+python ./line2line_point_sim.py -x 30.0 -y 30.0 -z 17.0 -r 0.45 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/line2line/
+python ./line2line_point_sim.py -x 30.0 -y 30.0 -z 18.0 -r 0.45 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/line2line/
+python ./line2line_point_sim.py -x 30.0 -y 30.0 -z 19.0 -r 0.45 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/line2line/
+python ./line2line_point_sim.py -x 30.0 -y 30.0 -z 20.0 -r 0.45 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/line2line/
+python ./line2line_point_sim.py -x 30.0 -y 30.0 -z 21.0 -r 0.45 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/line2line/
+python ./line2line_point_sim.py -x 30.0 -y 30.0 -z 22.0 -r 0.45 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/line2line/
+python ./line2line_point_sim.py -x 30.0 -y 30.0 -z 23.0 -r 0.45 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/line2line/
+python ./line2line_point_sim.py -x 30.0 -y 30.0 -z 24.0 -r 0.45 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/line2line/
+python ./line2line_point_sim.py -x 30.0 -y 30.0 -z 25.0 -r 0.45 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/line2line/
+python ./line2line_point_sim.py -x 30.0 -y 30.0 -z 26.0 -r 0.45 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/line2line/
+python ./line2line_point_sim.py -x 30.0 -y 30.0 -z 27.0 -r 0.45 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/line2line/
+python ./line2line_point_sim.py -x 30.0 -y 30.0 -z 28.0 -r 0.45 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/line2line/
+python ./line2line_point_sim.py -x 30.0 -y 30.0 -z 29.0 -r 0.45 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/line2line/
+python ./line2line_point_sim.py -x 30.0 -y 30.0 -z 30.0 -r 0.45 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/line2line/
+
+# via2line, r=0.45
+python ./via2line_point_sim.py --vm-offset 0.0 --ll-space 10.5 --via-dim-x 10.5 --via-dim-y 10.5 --via-dim-z 21.0 --line-dim-y 21.0 --line-dim-z 21.0 -r 0.45 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/via2line/
+python ./via2line_point_sim.py --vm-offset 0.5 --ll-space 10.5 --via-dim-x 10.5 --via-dim-y 10.5 --via-dim-z 21.0 --line-dim-y 21.0 --line-dim-z 21.0 -r 0.45 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/via2line/
+python ./via2line_point_sim.py --vm-offset 1.0 --ll-space 10.5 --via-dim-x 10.5 --via-dim-y 10.5 --via-dim-z 21.0 --line-dim-y 21.0 --line-dim-z 21.0 -r 0.45 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/via2line/
+python ./via2line_point_sim.py --vm-offset 1.5 --ll-space 10.5 --via-dim-x 10.5 --via-dim-y 10.5 --via-dim-z 21.0 --line-dim-y 21.0 --line-dim-z 21.0 -r 0.45 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/via2line/
+python ./via2line_point_sim.py --vm-offset 2.0 --ll-space 10.5 --via-dim-x 10.5 --via-dim-y 10.5 --via-dim-z 21.0 --line-dim-y 21.0 --line-dim-z 21.0 -r 0.45 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/via2line/
+python ./via2line_point_sim.py --vm-offset 2.5 --ll-space 10.5 --via-dim-x 10.5 --via-dim-y 10.5 --via-dim-z 21.0 --line-dim-y 21.0 --line-dim-z 21.0 -r 0.45 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/via2line/
+python ./via2line_point_sim.py --vm-offset 3.0 --ll-space 10.5 --via-dim-x 10.5 --via-dim-y 10.5 --via-dim-z 21.0 --line-dim-y 21.0 --line-dim-z 21.0 -r 0.45 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/via2line/
+python ./via2line_point_sim.py --vm-offset 3.5 --ll-space 10.5 --via-dim-x 10.5 --via-dim-y 10.5 --via-dim-z 21.0 --line-dim-y 21.0 --line-dim-z 21.0 -r 0.45 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/via2line/
+python ./via2line_point_sim.py --vm-offset 4.0 --ll-space 10.5 --via-dim-x 10.5 --via-dim-y 10.5 --via-dim-z 21.0 --line-dim-y 21.0 --line-dim-z 21.0 -r 0.45 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/via2line/
+python ./via2line_point_sim.py --vm-offset 4.5 --ll-space 10.5 --via-dim-x 10.5 --via-dim-y 10.5 --via-dim-z 21.0 --line-dim-y 21.0 --line-dim-z 21.0 -r 0.45 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/via2line/
+python ./via2line_point_sim.py --vm-offset 5.0 --ll-space 10.5 --via-dim-x 10.5 --via-dim-y 10.5 --via-dim-z 21.0 --line-dim-y 21.0 --line-dim-z 21.0 -r 0.45 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/via2line/
+python ./via2line_point_sim.py --vm-offset 5.5 --ll-space 10.5 --via-dim-x 10.5 --via-dim-y 10.5 --via-dim-z 21.0 --line-dim-y 21.0 --line-dim-z 21.0 -r 0.45 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/via2line/
+python ./via2line_point_sim.py --vm-offset 6.0 --ll-space 10.5 --via-dim-x 10.5 --via-dim-y 10.5 --via-dim-z 21.0 --line-dim-y 21.0 --line-dim-z 21.0 -r 0.45 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/via2line/
+python ./via2line_point_sim.py --vm-offset 6.5 --ll-space 10.5 --via-dim-x 10.5 --via-dim-y 10.5 --via-dim-z 21.0 --line-dim-y 21.0 --line-dim-z 21.0 -r 0.45 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/via2line/
+python ./via2line_point_sim.py --vm-offset 7.0 --ll-space 10.5 --via-dim-x 10.5 --via-dim-y 10.5 --via-dim-z 21.0 --line-dim-y 21.0 --line-dim-z 21.0 -r 0.45 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/via2line/
+python ./via2line_point_sim.py --vm-offset 7.5 --ll-space 10.5 --via-dim-x 10.5 --via-dim-y 10.5 --via-dim-z 21.0 --line-dim-y 21.0 --line-dim-z 21.0 -r 0.45 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/via2line/
+python ./via2line_point_sim.py --vm-offset 8.0 --ll-space 10.5 --via-dim-x 10.5 --via-dim-y 10.5 --via-dim-z 21.0 --line-dim-y 21.0 --line-dim-z 21.0 -r 0.45 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/via2line/
+python ./via2line_point_sim.py --vm-offset 8.5 --ll-space 10.5 --via-dim-x 10.5 --via-dim-y 10.5 --via-dim-z 21.0 --line-dim-y 21.0 --line-dim-z 21.0 -r 0.45 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/via2line/
+
+# line2line, r=0.25
+python ./line2line_point_sim.py -x 30.0 -y 30.0 -z  4.0 -r 0.25 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/line2line/
+python ./line2line_point_sim.py -x 30.0 -y 30.0 -z  5.0 -r 0.25 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/line2line/
+python ./line2line_point_sim.py -x 30.0 -y 30.0 -z  6.0 -r 0.25 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/line2line/
+python ./line2line_point_sim.py -x 30.0 -y 30.0 -z  7.0 -r 0.25 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/line2line/
+python ./line2line_point_sim.py -x 30.0 -y 30.0 -z  8.0 -r 0.25 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/line2line/
+python ./line2line_point_sim.py -x 30.0 -y 30.0 -z  9.0 -r 0.25 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/line2line/
+python ./line2line_point_sim.py -x 30.0 -y 30.0 -z 10.0 -r 0.25 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/line2line/
+python ./line2line_point_sim.py -x 30.0 -y 30.0 -z 11.0 -r 0.25 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/line2line/
+python ./line2line_point_sim.py -x 30.0 -y 30.0 -z 12.0 -r 0.25 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/line2line/
+python ./line2line_point_sim.py -x 30.0 -y 30.0 -z 13.0 -r 0.25 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/line2line/
+python ./line2line_point_sim.py -x 30.0 -y 30.0 -z 14.0 -r 0.25 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/line2line/
+python ./line2line_point_sim.py -x 30.0 -y 30.0 -z 15.0 -r 0.25 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/line2line/
+python ./line2line_point_sim.py -x 30.0 -y 30.0 -z 16.0 -r 0.25 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/line2line/
+python ./line2line_point_sim.py -x 30.0 -y 30.0 -z 17.0 -r 0.25 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/line2line/
+python ./line2line_point_sim.py -x 30.0 -y 30.0 -z 18.0 -r 0.25 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/line2line/
+python ./line2line_point_sim.py -x 30.0 -y 30.0 -z 19.0 -r 0.25 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/line2line/
+python ./line2line_point_sim.py -x 30.0 -y 30.0 -z 20.0 -r 0.25 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/line2line/
+python ./line2line_point_sim.py -x 30.0 -y 30.0 -z 21.0 -r 0.25 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/line2line/
+python ./line2line_point_sim.py -x 30.0 -y 30.0 -z 22.0 -r 0.25 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/line2line/
+python ./line2line_point_sim.py -x 30.0 -y 30.0 -z 23.0 -r 0.25 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/line2line/
+python ./line2line_point_sim.py -x 30.0 -y 30.0 -z 24.0 -r 0.25 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/line2line/
+python ./line2line_point_sim.py -x 30.0 -y 30.0 -z 25.0 -r 0.25 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/line2line/
+python ./line2line_point_sim.py -x 30.0 -y 30.0 -z 26.0 -r 0.25 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/line2line/
+python ./line2line_point_sim.py -x 30.0 -y 30.0 -z 27.0 -r 0.25 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/line2line/
+python ./line2line_point_sim.py -x 30.0 -y 30.0 -z 28.0 -r 0.25 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/line2line/
+python ./line2line_point_sim.py -x 30.0 -y 30.0 -z 29.0 -r 0.25 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/line2line/
+python ./line2line_point_sim.py -x 30.0 -y 30.0 -z 30.0 -r 0.25 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/line2line/
+
+# via2line, r=0.25
+python ./via2line_point_sim.py --vm-offset 0.0 --ll-space 10.5 --via-dim-x 10.5 --via-dim-y 10.5 --via-dim-z 21.0 --line-dim-y 21.0 --line-dim-z 21.0 -r 0.25 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/via2line/
+python ./via2line_point_sim.py --vm-offset 0.5 --ll-space 10.5 --via-dim-x 10.5 --via-dim-y 10.5 --via-dim-z 21.0 --line-dim-y 21.0 --line-dim-z 21.0 -r 0.25 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/via2line/
+python ./via2line_point_sim.py --vm-offset 1.0 --ll-space 10.5 --via-dim-x 10.5 --via-dim-y 10.5 --via-dim-z 21.0 --line-dim-y 21.0 --line-dim-z 21.0 -r 0.25 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/via2line/
+python ./via2line_point_sim.py --vm-offset 1.5 --ll-space 10.5 --via-dim-x 10.5 --via-dim-y 10.5 --via-dim-z 21.0 --line-dim-y 21.0 --line-dim-z 21.0 -r 0.25 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/via2line/
+python ./via2line_point_sim.py --vm-offset 2.0 --ll-space 10.5 --via-dim-x 10.5 --via-dim-y 10.5 --via-dim-z 21.0 --line-dim-y 21.0 --line-dim-z 21.0 -r 0.25 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/via2line/
+python ./via2line_point_sim.py --vm-offset 2.5 --ll-space 10.5 --via-dim-x 10.5 --via-dim-y 10.5 --via-dim-z 21.0 --line-dim-y 21.0 --line-dim-z 21.0 -r 0.25 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/via2line/
+python ./via2line_point_sim.py --vm-offset 3.0 --ll-space 10.5 --via-dim-x 10.5 --via-dim-y 10.5 --via-dim-z 21.0 --line-dim-y 21.0 --line-dim-z 21.0 -r 0.25 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/via2line/
+python ./via2line_point_sim.py --vm-offset 3.5 --ll-space 10.5 --via-dim-x 10.5 --via-dim-y 10.5 --via-dim-z 21.0 --line-dim-y 21.0 --line-dim-z 21.0 -r 0.25 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/via2line/
+python ./via2line_point_sim.py --vm-offset 4.0 --ll-space 10.5 --via-dim-x 10.5 --via-dim-y 10.5 --via-dim-z 21.0 --line-dim-y 21.0 --line-dim-z 21.0 -r 0.25 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/via2line/
+python ./via2line_point_sim.py --vm-offset 4.5 --ll-space 10.5 --via-dim-x 10.5 --via-dim-y 10.5 --via-dim-z 21.0 --line-dim-y 21.0 --line-dim-z 21.0 -r 0.25 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/via2line/
+python ./via2line_point_sim.py --vm-offset 5.0 --ll-space 10.5 --via-dim-x 10.5 --via-dim-y 10.5 --via-dim-z 21.0 --line-dim-y 21.0 --line-dim-z 21.0 -r 0.25 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/via2line/
+python ./via2line_point_sim.py --vm-offset 5.5 --ll-space 10.5 --via-dim-x 10.5 --via-dim-y 10.5 --via-dim-z 21.0 --line-dim-y 21.0 --line-dim-z 21.0 -r 0.25 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/via2line/
+python ./via2line_point_sim.py --vm-offset 6.0 --ll-space 10.5 --via-dim-x 10.5 --via-dim-y 10.5 --via-dim-z 21.0 --line-dim-y 21.0 --line-dim-z 21.0 -r 0.25 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/via2line/
+python ./via2line_point_sim.py --vm-offset 6.5 --ll-space 10.5 --via-dim-x 10.5 --via-dim-y 10.5 --via-dim-z 21.0 --line-dim-y 21.0 --line-dim-z 21.0 -r 0.25 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/via2line/
+python ./via2line_point_sim.py --vm-offset 7.0 --ll-space 10.5 --via-dim-x 10.5 --via-dim-y 10.5 --via-dim-z 21.0 --line-dim-y 21.0 --line-dim-z 21.0 -r 0.25 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/via2line/
+python ./via2line_point_sim.py --vm-offset 7.5 --ll-space 10.5 --via-dim-x 10.5 --via-dim-y 10.5 --via-dim-z 21.0 --line-dim-y 21.0 --line-dim-z 21.0 -r 0.25 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/via2line/
+python ./via2line_point_sim.py --vm-offset 8.0 --ll-space 10.5 --via-dim-x 10.5 --via-dim-y 10.5 --via-dim-z 21.0 --line-dim-y 21.0 --line-dim-z 21.0 -r 0.25 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/via2line/
+python ./via2line_point_sim.py --vm-offset 8.5 --ll-space 10.5 --via-dim-x 10.5 --via-dim-y 10.5 --via-dim-z 21.0 --line-dim-y 21.0 --line-dim-z 21.0 -r 0.25 -n 1000 --randseq-entropy 1234 --cpu-num 64 --save-path ./exp/percolation_points/gen_data/via2line/
